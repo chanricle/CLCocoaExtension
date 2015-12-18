b9d14ada9eca6d09f496a18781967cceee2dc928
ChanricleKing <chanricle@icloud.com>
2015-12-19 01:00:02 +0800

    add some category

---
+++ CLCocoaExtension/CLCocoaExtension/CLCocoaExtension/CLCocoaExtension.h
@@ -0,0 +1,18 @@
+//
+//  CLCocoaExtension.h
+//  CLCocoaExtension
+//
+//  Created by Chanricle on 12/18/15.
+//  Copyright © 2015 chanricle. All rights reserved.
+//
+
+#ifndef CLCocoaExtension_h
+#define CLCocoaExtension_h
+
+#import "UIColor+CLCocoaExtension.h"
+#import "UIView+CLCocoaExtension.h"
+#import "UIImage+CLCocoaExtension.h"
+#import "UIButton+CLCocoaExtension.h"
+#import "NSString+CLCocoaExtension.h"
+
+#endif /* CLCocoaExtension_h */
