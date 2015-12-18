a6976ec8946a8f2d6fe5ca48dda6a4f95e23ba53
ChanricleKing <chanricle@icloud.com>
2015-12-18 13:58:32 +0800

    init proj

---
+++ CLCocoaExtension/CLCocoaExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLCocoaExtension
+//
+//  Created by Chanricle on 12/18/15.
+//  Copyright © 2015 chanricle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLCocoaExtension/CLCocoaExtension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLCocoaExtension
+//
+//  Created by Chanricle on 12/18/15.
+//  Copyright © 2015 chanricle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
