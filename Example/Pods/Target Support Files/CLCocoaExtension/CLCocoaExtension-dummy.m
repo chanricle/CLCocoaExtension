a47ec8485e33aabb4ba9305f5d423f3ccf248988
ChanricleKing <chanricle@icloud.com>
2016-01-06 09:51:52 +0800

    clear old proj

--- Example/Pods/Target Support Files/CLCocoaExtension/CLCocoaExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CLCocoaExtension : NSObject
-@end
-@implementation PodsDummy_CLCocoaExtension
-@end
--- Example/Pods/Target Support Files/Pods-CLCocoaExtension_Example/Pods-CLCocoaExtension_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CLCocoaExtension_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CLCocoaExtension_ExampleVersionString[];
-
